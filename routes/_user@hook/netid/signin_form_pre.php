7a57cacacce4c2f405e55d64e446406ab96d2d58
Joby Elliott <jelliott@cs.unm.edu>
2021-03-08 11:50:09 -0700

    provide link to retry authentication on first exception

--- routes/_user@hook/netid/signin_form_pre.php
+++ routes/_user@hook/netid/signin_form_pre.php
@@ -1,5 +1,5 @@
 <?php
-include __DIR__.'/cas.php';
+include __DIR__ . '/cas.php';
 
 //fail gracefully if CAS isn't found
 if (!class_exists('\\phpCAS')) {
@@ -8,13 +8,25 @@ if (!class_exists('\\phpCAS')) {
 }
 
 //force authentication
-if (!phpCAS::isAuthenticated()) {
-    \phpCAS::forceAuthentication();
+try {
+    if (!phpCAS::isAuthenticated()) {
+        \phpCAS::forceAuthentication();
+    }
+} catch (\Throwable $th) {
+    if ($package['url.args.uniqid']) {
+        throw $th;
+    }
+    $url = $package->url();
+    $url['args.uniqid'] = uniqid();
+    $cms->helper('notifications')->printError(
+        '<strong>Sign-in error</strong><br>' .
+        "Signing in failed. Usually this is a temporary error caused by an expired token in UNM's central authentication system. Please <a href='$url'>try again</a>."
+    );
 }
 
 //get user
 if (\phpCAS::getUser()) {
-    $this->helper('users')->id(\phpCAS::getUser().'@netid');
+    $this->helper('users')->id(\phpCAS::getUser() . '@netid');
 } else {
     $this->helper('notifications')->error('Couldn\'t getUser() from phpCAS');
 }
