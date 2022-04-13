7a9ef9321eee57d5c670e9f88f39ea87509cb8d6
Joby Elliott <elhober@unm.edu>
2022-04-13 13:52:50 -0600

    enable poor man's cron

--- templates/default.php
+++ templates/default.php
@@ -5,6 +5,7 @@ are not some sort of error or special case.
 */
 
 use DigraphCMS\Context;
+use DigraphCMS\Cron\Cron;
 use DigraphCMS\UI\ActionMenu;
 use DigraphCMS\UI\Breadcrumb;
 use DigraphCMS\UI\Notifications;
@@ -49,6 +50,7 @@ use DigraphCMS\UI\UserMenu;
     </main>
     <?php
     echo Templates::render('sections/footer.php');
+    echo Cron::renderPoorMansCron();
     ?>
 </body>
 
