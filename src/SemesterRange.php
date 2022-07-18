337b7211b89c0c05147dbf1eb9ef5cf174580154
Joby Elliott <code@byjoby.com>
2022-07-18 13:23:46 -0600

    semester range class

---
+++ src/SemesterRange.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DigraphCMS_Plugins\unmous\ous_digraph_module;
+
+class SemesterRange
+{
+    protected $start, $end;
+
+    /**
+     * Entering null for the start or end makes this range extend indefinitely,
+     * for example entering null for $start would make all semesters up to $end
+     * match, and leaving both start and end null would make a range containing
+     * all semesters.
+     *
+     * @param Semester|null $start
+     * @param Semester|null $end
+     */
+    public function __construct(?Semester $start, ?Semester $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    public function start(): ?Semester
+    {
+        return $this->start;
+    }
+
+    public function end(): ?Semester
+    {
+        return $this->end;
+    }
+
+    public function contains(?Semester $semester): bool
+    {
+        if (!$semester) return false;
+        elseif ($this->start && $semester->isBefore($this->start)) return false;
+        elseif ($this->end && $semester->isAfter($this->end)) return false;
+        else return true;
+    }
+}
