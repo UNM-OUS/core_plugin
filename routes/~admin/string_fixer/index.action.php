5d79990388230bbf9d2206da224f923228ddcb07
Joby Elliott <code@byjoby.com>
2022-07-27 11:56:08 -0600

    global, shared string fixer database

---
+++ routes/~admin/string_fixer/index.action.php
@@ -0,0 +1,62 @@
+<h1>OUS string fixer</h1>
+<p>
+    This is a general purpose tool for correcting common sources of ugly strings.
+    Most of the corrections here are used to correct ugly degree abbreviations from Banner, or ugly org info from faculty/staff lists.
+    New terms that need to have their output value confirmed are listed at the top.
+</p>
+<p>
+    Note that these rules are generally applied at the time of import for performance reasons.
+    This means that updating things here will not retroactively update things like degree or faculty/staff lists that have already been imported.
+</p>
+<?php
+
+use DigraphCMS\HTML\Forms\FormWrapper;
+use DigraphCMS\HTML\Forms\INPUT;
+use DigraphCMS\HTML\Icon;
+use DigraphCMS\HTTP\RefreshException;
+use DigraphCMS\UI\Pagination\PaginatedTable;
+use DigraphCMS_Plugins\unmous\ous_digraph_module\SharedDB;
+
+$rules = SharedDB::query()->from('stringfix')
+    ->order('needs_review ASC')
+    ->order('COALESCE(output,input)');
+
+$table = new PaginatedTable(
+    $rules,
+    function (array $row) {
+        $id = md5(serialize([$row['category'], $row['input']]));
+        // set up form
+        $form = new FormWrapper($id);
+        $form->addClass('inline-form');
+        $form->setData('target', '_frame');
+        $input = new INPUT($id);
+        $input->setDefault($row['output']);
+        $form->addChild($input);
+        // give button the right text
+
+        // add callback
+        $form->addCallback(function () use ($row, $input) {
+            SharedDB::query()->update('stringfix', [
+                'output' => $input->value(),
+                'needs_review' => false,
+            ])->where('category', $row['category'])
+                ->where('input', $row['input'])
+                ->execute();
+            throw new RefreshException();
+        });
+        return [
+            $row['needs_review'] ? new Icon('star') : '',
+            $row['category'],
+            $row['input'],
+            $form
+        ];
+    },
+    [
+        '',
+        'Category',
+        'Input',
+        'Output',
+    ]
+);
+
+echo $table;
---
+++ src/StringFixer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace DigraphCMS_Plugins\unmous\ous_digraph_module;
+
+class StringFixer
+{
+    public static function college(?string $name): ?string
+    {
+        return static::runFixers($name, ['college']);
+    }
+
+    public static function department(?string $name): ?string
+    {
+        return static::runFixers($name, ['department']);
+    }
+
+    public static function program(?string $name): ?string
+    {
+        return static::runFixers($name, ['program']);
+    }
+
+    public static function major(?string $name): ?string
+    {
+        return static::runFixers($name, ['major']);
+    }
+
+    public static function runFixers(?string $input, array $categories): ?string
+    {
+        $input = trim($input);
+        if (!$input) return null;
+        foreach ($categories as $c) {
+            if ($output = static::run($input, $c)) return $output;
+        }
+        return $input;
+    }
+
+    protected static function run(string $input, string $category): string
+    {
+        static $cache = [];
+        $id = md5(serialize([$input, $category]));
+        if (!isset($cache[$id])) {
+            // look for a match
+            $result = SharedDB::query()->from('stringfix')
+                ->where('input', $input)
+                ->where('category', $category)
+                ->limit(1)
+                ->fetch();
+            // add result to cache if it exists
+            if ($result) $cache[$id] = $result['output'];
+            else {
+                // otherwise save a null in the cache
+                $cache[$id] = null;
+                // if there is no match, add this to stringfix with the 'needs_review' flag
+                SharedDB::query()->insertInto(
+                    'stringfix',
+                    [
+                        'input' => $input,
+                        'output' => $input,
+                        'category' => $category,
+                        'needs_review' => true
+                    ]
+                )->execute();
+            }
+        }
+        return $cache[$id] ?? $input;
+    }
+}
