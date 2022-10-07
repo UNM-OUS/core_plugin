67dcb57c7f5983075f7218927039fe4e7710c801
Joby Elliott <code@byjoby.com>
2022-10-07 08:33:46 -0600

    further tweaked semester range strings

--- src/SemesterRange.php
+++ src/SemesterRange.php
@@ -24,11 +24,11 @@ class SemesterRange
     public function __toString()
     {
         if (!$this->start && !$this->end) return 'any semester';
-        elseif (!$this->start) return 'no later than ' . $this->end;
-        elseif (!$this->end) return 'no earlier than ' . $this->start;
+        elseif (!$this->start) return $this->end . ' or earlier';
+        elseif (!$this->end) return $this->start . ' or later';
         elseif ($this->end == $this->start->next()) {
-            if ($this->start->year() == $this->end->year()) return sprintf('%s or %s %s', $this->start->semester(), $this->end->semester(), $this->end->year());
-            else return $this->start . ' or ' . $this->end;
+            if ($this->start->year() == $this->end->year()) return sprintf('%s and %s %s', $this->start->semester(), $this->end->semester(), $this->end->year());
+            else return $this->start . ' and ' . $this->end;
         } else {
             if ($this->start->year() == $this->end->year()) return sprintf('%s to %s %s', $this->start->semester(), $this->end->semester(), $this->end->year());
             else return $this->start . ' to ' . $this->end;
