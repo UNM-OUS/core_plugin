7fd33a011e075821119ca047b77be131dc050932
Joby Elliott <elhober@unm.edu>
2023-01-30 10:36:51 -0700

    use curl for group info

--- src/Users/OUSGroupSource.php
+++ src/Users/OUSGroupSource.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Digraph\Modules\ous_digraph_module\Users;
 
 use Digraph\Users\GroupSources\AbstractGroupSource;
 
 class OUSGroupSource extends AbstractGroupSource
 {
-    public function groups(string $id) : ?array
+    public function groups(string $id): ?array
     {
         //if ID isn't a NetID, short circuit, this source knows nothing
         $id = explode('@', $id);
@@ -16,19 +17,24 @@ class OUSGroupSource extends AbstractGroupSource
         $netid = array_shift($id);
         //check cache
         $cache = $this->cms->cache();
-        $cacheID = md5(static::class).'.'.$netid;
+        $cacheID = md5(static::class) . '.' . $netid;
         $groups = $cache->getItem($cacheID);
         //load and save into cache if cache isn't hit
         if (!$groups->isHit()) {
-            $url = 'https://secretary.unm.edu/groups/index.php?netid='.$netid;
-            if ($data = json_decode(file_get_contents($url))) {
+            $url = 'https://secretary.unm.edu/groups/index.php?netid=' . $netid;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            if ($data = json_decode(curl_exec($ch))) {
                 $groups->set($data);
                 $groups->expiresAfter(3600);
                 $cache->save($groups);
             }
+            curl_close($ch);
         }
         //return
         $groups = $groups->get();
-        return $groups?$groups:[];
+        return $groups ? $groups : [];
     }
 }
