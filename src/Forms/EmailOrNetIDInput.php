92990f09cc24800f9233c5a0e7b317295abbccfe
Joby Elliott <code@byjoby.com>
2023-03-30 08:27:28 -0600

    allow netIDs to start with an underscore, so we can make up invalid ones that definitely don't exist

--- src/Forms/EmailOrNetIDInput.php
+++ src/Forms/EmailOrNetIDInput.php
@@ -24,7 +24,7 @@ class EmailOrNetIDInput extends INPUT
                 if (preg_match('/^[0-9]{9}$/', $this->value())) {
                     return "Please enter a NetID username, not a Banner ID number";
                 }
-                if (!preg_match('/^[a-z].{1,19}$/', $this->value())) {
+                if (!preg_match('/^[a-z_].{1,19}$/', $this->value())) {
                     return "NetIDs must be 2-20 characters and begin with a letter";
                 }
                 if (preg_match('/[^a-z0-9_]/', $this->value())) {
