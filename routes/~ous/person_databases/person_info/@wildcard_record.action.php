442016d0d4eeb16ec4fd56130a50edf4fbb58d8d
Joby Elliott <jelliott@cs.unm.edu>
2024-10-15 10:21:16 -0600

    make personinfo edit form work

--- routes/~ous/person_databases/person_info/@wildcard_record.action.php
+++ routes/~ous/person_databases/person_info/@wildcard_record.action.php
@@ -4,6 +4,8 @@ use DigraphCMS\CodeMirror\YamlArrayInput;
 use DigraphCMS\Context;
 use DigraphCMS\HTML\Forms\Field;
 use DigraphCMS\HTML\Forms\FormWrapper;
+use DigraphCMS\HTTP\RefreshException;
+use DigraphCMS\UI\Notifications;
 use DigraphCMS_Plugins\unmous\ous_digraph_module\PersonInfo;
 
 $person = PersonInfo::fetch(Context::url()->actionSuffix());
@@ -13,5 +15,12 @@ $form = new FormWrapper();
 $data = (new Field('User data', new YamlArrayInput()))
     ->setDefault($person->get())
     ->addForm($form);
+
+if ($form->ready()) {
+    $person->set(null, $data->value());
+    $person->save();
+    Notifications::flashConfirmation('PersonInfo record updated');
+    throw new RefreshException();
+}
 
 echo $form;
