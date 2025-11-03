47d09f172130a5bf8d5c13bd4b15fd48bd3a9c15
Joby Elliott <jelliott@cs.unm.edu>
2025-11-03 08:49:27 -0700

    adjustments to header sizing

--- templates/sections/header.php
+++ templates/sections/header.php
@@ -8,7 +8,7 @@ use DigraphCMS\URL\URL;
 $url = new URL('/');
 $hero = Media::get('/hero.jpg')
     ->image()
-    ->crop(1200, 400);
+    ->crop(1600, 400);
 $picture = new ResponsivePicture($hero, 'header hero image');
 $picture->setExpectedWidth(100);
 echo "<header id=\"header\">";
