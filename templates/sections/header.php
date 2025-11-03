877cd72ab28c50f5e83664fd833061d0bae8a72f
Joby Elliott <jelliott@cs.unm.edu>
2025-11-03 08:46:02 -0700

    updated header design for better performance

--- templates/sections/header.php
+++ templates/sections/header.php
@@ -1,11 +1,19 @@
 <?php
 
 use DigraphCMS\Context;
+use DigraphCMS\HTML\ResponsivePicture;
+use DigraphCMS\Media\Media;
 use DigraphCMS\URL\URL;
 
 $url = new URL('/');
+$hero = Media::get('/hero.jpg')
+    ->image()
+    ->crop(1200, 400);
+$picture = new ResponsivePicture($hero, 'header hero image');
+$picture->setExpectedWidth(100);
 echo "<header id=\"header\">";
 echo "<div id='header__wrapper'>";
-echo "<h1><a href='$url'>" . Context::fields()['site.name'] . "</a></h1>";
+echo "<div id='header__hero'>$picture</div>";
+echo "<div id='header__name'><h1><a href='$url'>" . Context::fields()['site.name'] . "</a></h1></div>";
 echo "</div>";
 echo "</header>";
