68359a8731f14c32660912541c311bcf6f26622b
Joby Elliott <code@joby.lol>
2026-05-28 08:09:09 -0600

    http to https links

--- templates/sections/footer.php
+++ templates/sections/footer.php
@@ -11,7 +11,7 @@ $logo = Media::get('/unm-footer-logo.png');
 <footer id="footer">
     <div class="unm-info">
         <h1>
-            <a href="http://www.unm.edu/">
+            <a href="https://www.unm.edu/">
                 <img src="<?= $logo->url() ?>" height="60" width="300" alt="The University of New Mexico">
             </a>
         </h1>
@@ -26,20 +26,20 @@ $logo = Media::get('/unm-footer-logo.png');
     <div class="unm-links">
         <p>
             <a href="https://www.facebook.com/universityofnewmexico" class="social-icon-link"><i class="fab fa-facebook"></i></a>
-            <a href="http://instagram.com/uofnm" class="social-icon-link"><i class="fab fa-instagram"></i></a>
+            <a href="https://instagram.com/uofnm" class="social-icon-link"><i class="fab fa-instagram"></i></a>
             <a href="https://twitter.com/unm" class="social-icon-link"><i class="fab fa-twitter"></i></a>
-            <a href="http://www.youtube.com/user/unmlive" class="social-icon-link"><i class="fab fa-youtube"></i></a>
+            <a href="https://www.youtube.com/user/unmlive" class="social-icon-link"><i class="fab fa-youtube"></i></a>
             <br>
             more at
-            <a href="http://social.unm.edu/">social.unm.edu</a>
+            <a href="https://social.unm.edu/">social.unm.edu</a>
         </p>
 
         <p>
-            <a href="http://www.unm.edu/accessibility.html">Accessibility</a>
-            <a href="http://www.unm.edu/legal.html">Legal</a>
-            <a href="http://www.unm.edu/contactunm.html">Contact UNM</a>
+            <a href="https://www.unm.edu/accessibility.html">Accessibility</a>
+            <a href="https://www.unm.edu/legal.html">Legal</a>
+            <a href="https://www.unm.edu/contactunm.html">Contact UNM</a>
             <a href="https://www.unm.edu/consumer-information/"><span>Consumer Information</span></a>
-            <a href="http://nmhedss2.state.nm.us/Dashboard/index.aspx?ID=21">New Mexico Higher Education Dashboard</a>
+            <a href="https://nmhedss2.state.nm.us/Dashboard/index.aspx?ID=21">New Mexico Higher Education Dashboard</a>
         </p>
 
         <!-- <p>
--- templates/unm/top-nav.php
+++ templates/unm/top-nav.php
@@ -6,13 +6,13 @@ use DigraphCMS\URL\URL;
 <div id="unm-top-nav">
     <div class="unm-top-nav__wrapper">
         <nav class="unm-navbar" role="complementary">
-            <a class="navbar-brand" href="http://www.unm.edu">The University of New Mexico</a>
+            <a class="navbar-brand" href="https://www.unm.edu">The University of New Mexico</a>
 
             <div class="menu">
                 <div class="unm-menubar">
-                    <a class="unm-menuitem" href="http://directory.unm.edu/departments/" title="UNM A to Z">UNM A-Z</a>
+                    <a class="unm-menuitem" href="https://directory.unm.edu/departments/" title="UNM A to Z">UNM A-Z</a>
                     <a class="unm-menuitem" href="https://my.unm.edu" title="myUNM">myUNM</a>
-                    <a class="unm-menuitem" href="http://directory.unm.edu" title="Directory">Directory</a>
+                    <a class="unm-menuitem" href="https://directory.unm.edu" title="Directory">Directory</a>
                 </div>
                 <!-- search form -->
                 <form action="<?php echo new URL('/search/'); ?>" id="unm_search_form" method="get">
