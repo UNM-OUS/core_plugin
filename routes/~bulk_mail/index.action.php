75f915e91c5c16f11b9d4ca3368a2e1caa13216b
Joby Elliott <jelliott@cs.unm.edu>
2026-06-25 08:30:05 -0600

    cleanup for bulk mail interface

--- routes/~bulk_mail/index.action.php
+++ routes/~bulk_mail/index.action.php
@@ -4,11 +4,9 @@
 use DigraphCMS\UI\Format;
 use DigraphCMS\UI\Pagination\ColumnStringFilteringHeader;
 use DigraphCMS\UI\Pagination\PaginatedTable;
-use DigraphCMS\UI\Sidebar\Sidebar;
 use DigraphCMS\URL\URL;
 use DigraphCMS_Plugins\unmous\ous_digraph_module\BulkMail\BulkMail;
 use DigraphCMS_Plugins\unmous\ous_digraph_module\BulkMail\Mailing;
-use DigraphCMS_Plugins\unmous\ous_digraph_module\Semesters;
 
 echo "<h2>Templates</h2>";
 printf('<a href="%s" class="button">Create bulk mail template</a>', new URL('_create.html'));
@@ -53,43 +51,3 @@ echo new PaginatedTable(
         'Sent',
     ],
 );
-
-// look for and surface relevant past mailings from this time last year
-
-// this time last equivalent semester
-Sidebar::add(function (): string {
-    $last_semester = Semesters::current()->previous(3);
-    $relevant = BulkMail::mailings();
-    $other_day = $start = Semesters::transferTime(
-        time(),
-        $last_semester,
-        Semesters::current(),
-    );
-    $start = (clone $other_day)->sub(new DateInterval('P1W'));
-    $end = (clone $other_day)->add(new DateInterval('P2W'));
-    $relevant
-        ->where(
-            'sent > ?',
-            $start->getTimestamp(),
-        )->where(
-            'sent < ?',
-            $end->getTimestamp(),
-        );
-    // if ($relevant->count() == 0) return null;
-    return sprintf(
-        '<h1>This time %s</h1><div class="small">%s to %s</div>%s',
-        $last_semester,
-        Format::date($start),
-        Format::date($end),
-        new PaginatedTable(
-            $relevant,
-            function (Mailing $mailing): array {
-                return [
-                    $mailing->previewUrl()->html(),
-                    sprintf('<a href="%s">copy</a>', $mailing->copyUrl()),
-                    sprintf('<a href="%s">source</a>', $mailing->sourceUrl()),
-                ];
-            }
-        ),
-    );
-});
